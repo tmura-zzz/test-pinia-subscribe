8ee44de23a0819b515d670d428594571b311c5a7
Tadayoshi Murakami <tmura_zzz@da2.so-net.ne.jp>
2025-04-08 00:01:52 +0900

    settings for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/test-pinia-subscribe/",
   plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
     port: 4001,
     watch: {
